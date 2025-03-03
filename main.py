43d2ca8b0ae60dd7a60b097aac3e853f969ab1c9
CASEOHSTARTEDTHEETONFIRE <hari.raja@share.efacademy.org>
2025-03-02 18:02:49 -0800

    Edited Tic-Tac-Toe and made Secret Number Game

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 print("Hello, Casoh!")
 tic_tac_toe = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 print("- - - - - - - - - - - - - - -  ")
@@ -14,26 +15,24 @@ if piece == "O":
 #Input functions converts ur response in2 a string (str),
 #how to save a input function -- make a variable n store it AND SAVE FILE.
 print("hello " + name)
-print("--------------")
-print(str(tic_tac_toe[0]) + "|" + str(tic_tac_toe[1]) + "|" +
-    str(tic_tac_toe[2]))
-print("-----")
-print(str(tic_tac_toe[3]) + "|"+ str(tic_tac_toe[4])+"|"+str(tic_tac_toe[5]))
-print("-----")
-print(str(tic_tac_toe[6])+"|"+ str(tic_tac_toe[7]) +"|"+ str(tic_tac_toe[8]))
-position = input("where do u want to place your piece?")
-# input saves the data as strings. :)
 
-while position not in "1,2,3,4,5,6,7,8,9" and len(position) > 1:
-  print("pls choose a VALID response!!!!!!!!!! :)-")
-  position = input("where do u want to place your piece?")
+while True:
+    # prints the tic tac toe board
+    print("--------------")
+    print(str(tic_tac_toe[0]) + "|" + str(tic_tac_toe[1]) + "|" +
+        str(tic_tac_toe[2]))
+    print("-----")
+    print(str(tic_tac_toe[3]) + "|"+ str(tic_tac_toe[4])+"|"+str(tic_tac_toe[5]))
+    print("-----")4
+    print(str(tic_tac_toe[6])+"|"+ str(tic_tac_toe[7]) +"|"+ str(tic_tac_toe[8]))
+    position = input("where do u want to place your piece?")
+    # input saves the data as strings. :)
+    # keeps asking to put a valid response until the user puts a valid response
+    while position not in "1,2,3,4,5,6,7,8,9" and len(position) > 1:
+        print("pls choose a VALID response!!!!!!!!!! :-)")
+        position = input("where do u want to place your piece?")
+    # place the piece in the designated positionß
+    tic_tac_toe[int(position) - 1] = piece
+    os.system('clear')
 
-tic_tac_toe[int(position) - 1] = piece
 
-print("--------------")
-print(str(tic_tac_toe[0]) + "|" + str(tic_tac_toe[1]) + "|" +
-    str(tic_tac_toe[2]))
-print("-----")
-print(str(tic_tac_toe[3]) + "|"+ str(tic_tac_toe[4])+"|"+str(tic_tac_toe[5]))
-print("-----")
-print(str(tic_tac_toe[6])+"|"+ str(tic_tac_toe[7]) +"|"+ str(tic_tac_toe[8]))
---
+++ notes.py
@@ -0,0 +1,30 @@
+#import random
+
+#random_number = random.randint(1, 5)
+#print(random_number)
+
+# Create a guess the number game that asks the user to guess a number between 1 - 100
+# If the user guesses the number correctly, print "You win!"
+# If the user guesses the number incorrectly, print ""Higher" if the number you guess is low than the random number
+# If the user guesses the number incorrectly, print "Lower" if the number you guess is higher than the random number
+# If the user guesses the number incorrectly, print "You lose" if the user guesses the number incorrectly 5 times. 
+points= 5
+import random 
+random_number = random.randint(1,1000)
+while True:
+    answer= int(input("guess the number from 1-1000 you have 5 tries."))
+    if answer == random_number:
+        print("You Win!")
+        break
+    if answer < random_number:
+        print("womp womp. Guess higher.")
+        points -= 1
+    if answer > random_number:
+        print("womp womp. Guess lower.")
+        points -= 1
+
+    if points <= 0: 
+        print("you lose womp womp. ")
+        print("The correct answer was " + str(random_number))
+        break
+    
